9dbee9c867c68da831f22e93c7150e9bce9172f3
yakubk18 <golandazyakub@gmail.com>
2022-02-21 14:37:29 +0530

    React assignment 2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+  const { employee } = props;
+
+  return (
+    <div className="container">
+      <div className="profile-image">
+        <img src={employee.profileImg} alt="profile" />
+      </div>
+      <div className="tittle">
+        <h1>{employee.name}</h1>
+      </div>
+      <div className="profile-info">
+        <label>Location</label>
+        <strong>{employee.location}</strong>
+      </div>
+      <div className="profile-info">
+        <label>BloodGroup</label>
+        <strong>{employee.bloodGroup}</strong>
+      </div>
+      <div className="profile-info">
+        <label>age</label>
+        <strong>{employee.age}</strong>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default App;
